d04caf65f91ef9f5262147e9074a5e0612af0f51
m3vance <m3vance@gmail.com>
2023-04-30 18:17:05 -0700

    completed week 2 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -26,42 +26,162 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // Pseudo code:
+    // Input: an object
+    // Output: a string that states whether the object is divisible by three, or is not divisible by three
+    // Process: create a function using % 3 === 0 operator to determine if a number is divisible by 3
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
+describe("threeDivider", () => {
+    it(" takes a object as an argument and decides if the number inside it is evenly divisible by three or not", () => {
+    const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
+    const object2 = { number: 0 }
 // Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
+    const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
+        expect(threeDivider(object1.number)).toEqual("15 is divisible by three")
+        expect(threeDivider(object2.number)).toEqual("0 is divisible by three")
+        expect(threeDivider(object3.number)).toEqual("-7 is not divisible by three")
+        })
+})
+
+// test output: threeDivider
+    // ✕  takes a object as an argument and decides if the number inside it is evenly divisible by three or not (1 ms)
+
+    // ● threeDivider ›  takes a object as an argument and decides if the number inside it is evenly divisible by three or not
+  
+    //   ReferenceError: threeDivider is not defined
 
 // b) Create the function that makes the test pass.
+
+// Green:
+// const threeDivider = (object) => {
+//     if(object % 3 === 0)
+//     return `${object} is divisible by three`
+//     else 
+//     return `${object} is not divisible by three`
+// }
+
+// Refactor:
+const threeDivider = (object) => {
+    return object % 3 === 0 ? `${object} is divisible by three` : `${object} is not divisible by three`
+}
+
+// output: ./code-challenges.test.js
+//   threeDivider
+//   ✓  takes a object as an argument and decides if the number inside it is evenly divisible by three or not (3 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // Pseudo code:
+    // Input: an array of words
+    // Output: an array with all the words capitalized
+    // Process: create a function using the .map higher order function and .toUpperCase method to output all the given words capitalized
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("upperCaser", () => {
+    it("takes in an array of words and returns an array with all the words capitalized", () => {
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+        expect(upperCaser(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(upperCaser(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+    })
+})
+
+// test output: threeDivider
+    // ✓  takes a object as an argument and decides if the number inside it is evenly divisible by three or not (2 ms)
+    // upperCaser
+    //   ✕ takes in an array of words and returns an array with all the words capitalized
+  
+    // ● upperCaser › takes in an array of words and returns an array with all the words capitalized
+  
+    //   ReferenceError: upperCaser is not defined
 
 // b) Create the function that makes the test pass.
 
+
+// Green: 
+// const upperCaser = (array) => {
+//     let newArray = []
+//     for (let i = 0; i < array.length; i++) {
+//         newArray.push(array[i].charAt(0).toUpperCase() + array[i].slice(1))
+//     }
+//     return newArray
+// }
+
+// Refactor:
+const upperCaser = (array) => {
+    return array.map(value => value[0].toUpperCase() + value.substring(1))
+}
+
+// Notes: My original function was actually the .map refactor. It was very intuitive. I had to search for help, for the 'for loop' version of the function. The built in methods got confusing compared to the .map function. 
+
+// // output: PASS  ./code-challenges.test.js
+//   threeDivider
+//   ✓  takes a object as an argument and decides if the number inside it is evenly divisible by three or not (3 ms)
+// upperCaser
+//   ✓ takes in an array of words and returns an array with all the words capitalized (1 ms)
+
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
-// Pseudo code:
+// Pseudo code: 
+    // Input: a string
+    // Output: the index number of the first vowel in the input string
+    // Process: create a function that uses a for loop to iterate through a string and checks whether the string contains any vowels from the variable vowels = ["a", "e", "i", "o", "u"]
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("firstVowel", () => {
+    it("takes in a string and logs the index of the first vowel", () => {
 const vowelTester1 = "learn"
 // Expected output: 1
 const vowelTester2 = "academy"
 // Expected output: 0
 const vowelTester3 = "challenges"
 // Expected output: 2
+        expect(firstVowel(vowelTester1)).toEqual(1)
+        expect(firstVowel(vowelTester2)).toEqual(0)
+        expect(firstVowel(vowelTester3)).toEqual(2)
+    })
+})
+
+// test output: FAIL  ./code-challenges.test.js
+//   threeDivider
+//   ✓  takes a object as an argument and decides if the number inside it is evenly divisible by three or not (2 ms)
+// upperCaser
+//   ✓ takes in an array of words and returns an array with all the words capitalized (1 ms)
+// firstVowel
+//   ✕ takes in a string and logs the index of the first vowel (1 ms)
+
+// ● firstVowel › takes in a string and logs the index of the first vowel
+
+//   ReferenceError: firstVowel is not defined
 
 // b) Create the function that makes the test pass.
+
+const vowels = ["a", "e", "i", "o", "u"]
+
+const firstVowel = (string) => {
+   for (let i = 0; i < string.length; i++) {
+        if (vowels.includes(string[i])) {
+            return i;
+        }
+   }}
+//  return vowels.includes(string[i]) ? i : "There is no vowel in this string" 
+
+// output:  PASS  ./code-challenges.test.js
+//   threeDivider
+//   ✓  takes a object as an argument and decides if the number inside it is evenly divisible by three or not (2 ms)
+// upperCaser
+//   ✓ takes in an array of words and returns an array with all the words capitalized
+// firstVowel
+//   ✓ takes in a string and logs the index of the first vowel
+
+// notes: I tried and tried to figure out a way to use a ternary operator for this function, but I cannot find a way to make it work. After doing a bit of research, it seems it wont work because the return is still inside the for loop, so I tried making the ternary into a variable, and calling it after the for loop, but still with no success. It possibly has something to do with the 'else' part of the ternary being attached?
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,61 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: A parameter is variable that's used in a function. An argument is the same thing, except its used when you are calling on the parameter. 
 
-Researched answer:
+Researched answer: Parameters are like placeholders in a function. The arguments are the actual data that replaces the parameters in a function when it is being called on.
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: .map requires index. The order is always index, value, array and must be in order, even when the prior ones aren't being used. value and array are optional.
 
-Researched answer:
+Researched answer: .map can also be used to reformat objects in an array.
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: .map will output the exact same number of objects in the array, while .filter will only output a subset of the array based on what parameters are passed.
 
-Researched answer:
+Researched answer: .filter will filter through each element in the array and return a filtered array. Map will output a new array, with the same number of elements as the original array. There is also forEach and reduce. forEach does the same thing as filter, except forEach will execute the function on each element, while filter will remove the element if the return value is false. Reduce will reduce the entire array to a single value.
 
 1. What is iteration?
 
-Your answer:
+Your answer: Iteration is the process of calling on each element of an array. Using a for loop, we can tell the for loop what index value to start at, where to end at, and the rate of the value it increases each time the for loop is executed.
 
-Researched answer:
+example: for (let i = 0; array.length; i++)
+
+The first part is index where the for loop will first execute. The second part is telling it where to stop. In this case, it will execute until it reaches the end of the array. The last part is shorthand for telling it to increase the value by 1 each time it executes, essentially going through each element, one by one.
+
+Researched answer: There are a few other iteration methods, like do.. while. do and while used together will execute a function on each element until it returns false.
 
 1. What is the difference between a function and a method?
 
-Your answer:
+Your answer: a function is a dynamic piece of code that can be written with parameters that are interchangeable. Functions are especially useful when we want to use a certain process multiple times with different arguments. They save space and make it easy for coders to repeat processes without writing seperate codes. Methods are prewritten functions that are attached to a .method. Some very nice people decided to write code for all of us and attach them to simple names, such as .toUpperCase, etc etc.
 
-Researched answer:
+Researched answer: A function is seperate from any method or object. Methods are associated with objects and are used only in conjunction with objects. A function can be seen as the skeleton of instructions, while the methods are instructions that are used on objects within the function.
 
 6. STRETCH: What is hoisting in JavaScript?
 
-Your answer:
+Your answer: Hoisting is calling on functions that are written outside of the coding file.
 
-Researched answer:
+Researched answer: I was totally wrong haha. Hoisting has to do with the scope. If a let or const variable is declared after the function, the function will not run. Using var however, will let it run, since var is read on the global scope. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Component:
+1. Component: Components are code snippets that are prewritten for us to use easily. They serve the same purpose as functions, but instead return HTML.
 
-2. Spread operator:
+2. Spread operator: The '...' spread operator can be used to combine arrays, or copy arrays to be used in a new variable. For example: 
 
-3. React state:
+const array1 = ["Thank",  "you", "for"]
+const array2 = [...array1, "grading", "this!"]
 
-4. React props:
+console.log(array2)
 
-5. DOM events:
+output: [ 'Thank', 'you', 'for', 'grading', 'this!' ]
+
+3. React state: React states are built in objects used to hold data that can change.
+
+4. React props: React props are the arguments passed in to react components. They are written using HTML attributes.
+
+5. DOM events: Document object model events are instructions for running a certain code when an event happens, such as clicking an image or button and a visual effect happening, or mousing over a certain part of the site and more info pops up.
